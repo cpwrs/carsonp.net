fbdea2db7df11ebba680a60a1a4861b57f33e733
Carson Powers <me@carsonp.net>
2025-04-05 01:16:09 -0500

    feat: nix run .#deploy

--- README.md
+++ README.md
@@ -1,20 +1,14 @@
 # carsonp.net
-
-### Deploy to Production
-For slow instances, build the OS locally and deploy to EC2: 
-```bash
-nixos-rebuild --target-host root@instance-ip --flake .#server switch
-```
-This NixOS config sets up nginx, grabs Let's Encrypt certificates, opens TCP ports, and defines a systemd service to start the web app.
-
-### Development
 Enter the dev environment with `nix develop`.
-Age recipients defined in `secrets.nix` can decrypt the secret environment variables:
-```bash
-age --decrypt -i ~/.ssh/id_ed25519 -o .env secrets/env.age
-source .env
-```
-Then, start the server with an optional host and port argument:
+Start the server locally with an optional host and port argument:
 ```bash
 python3 app/backend.py --host 127.0.0.1 --port 8000
 ```
+
+### Deploy
+Age recipients defined in `secrets.nix` have deploy privileges and can decrypt the prod environment secrets.
+Build the OS locally and deploy to the instance:
+```nix
+nix run .#deploy
+```
+This NixOS config sets up nginx, grabs Let's Encrypt certificates, opens TCP ports, and defines a systemd service to start the web app.
--- flake.nix
+++ flake.nix
@@ -31,10 +31,6 @@
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
-          # Encrypting secrets
-          age
-          agenix.packages.${system}.default
-
           # Language servers
           nil
           pyright
@@ -65,6 +61,41 @@
           agenix.nixosModules.default
           ./server.nix 
         ];
+      };
+
+      # Deployment script
+      packages.${system}.deploy = pkgs.writeShellApplication {
+        name = "deploy";
+        runtimeInputs = with pkgs; [
+          nixos-rebuild
+          agenix.packages.${system}.default
+          age
+        ];
+
+        text = ''
+          set -e
+
+          # Check if user is an age recipient
+          if ! agenix -d secrets/ip.age > /dev/null 2>&1; then
+            echo "Error: User does not have deploy privileges. Failed to decrypt secrets."
+            exit 1
+          fi
+
+          # Grab target private key
+          PEM_FILE=$(mktemp)
+          agenix -d secrets/pem.age > "$PEM_FILE"
+          chmod 600 "$PEM_FILE"
+
+          # Grab target IP
+          IP=$(agenix -d secrets/ip.age)
+
+          # Skip confirmation and don't save target info to known_hosts
+          export NIX_SSHOPTS="-i $PEM_FILE -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+          
+          echo "Deploying to $IP..."
+          # Deploy the NixOS config to target
+          nixos-rebuild --target-host "root@$IP" --flake .#server switch
+        '';
       };
     };
 }
